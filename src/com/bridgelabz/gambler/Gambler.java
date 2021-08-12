e0b3f6023577b73b7587c89b76af3fe6e49390dd
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 13:08:54 +0530

    [Ruchi]: Added win or lose after reaching condition

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -5,17 +5,28 @@ public class Gambler {
     public static final int $PERGAME = 1;
     public static final int WIN = 1;
     public static final int LOSE = 0;
+
     public static void main(String[] args) {
-        double Action = Math.floor(Math.random() * 10) % 2;
-        if(Action==WIN)
-        {
-            System.out.println("You won $1");
+        int totalamount = 100;
 
-        }
-        else if(Action==LOSE)
-        {
-            System.out.println("You lose $1");
-        }
+        while (totalamount>50 && totalamount<150 ) {
+            double Action = Math.floor(Math.random() * 10) % 2;
+            if ((int) Action == WIN) {
+                totalamount = totalamount + 1;
+                System.out.println("You won $1 your current stake value is : $" + totalamount);
 
+            } else if ((int) Action == LOSE) {
+                totalamount = totalamount - 1;
+                System.out.println("You lose $1 your current stake value is : $" + totalamount);
+            }
+        }
+        if(totalamount==50)
+        {
+            System.out.println("You lose");
+        }
+        else
+        {
+            System.out.println("You Won");
+        }
     }
 }
