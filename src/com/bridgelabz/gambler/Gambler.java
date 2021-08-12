3a8a4ac9ed44144ab02de0c615ddcc4552e2d53f
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 12:39:27 +0530

    [Ruchi]: Added Win or lose

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -3,7 +3,19 @@ package com.bridgelabz.gambler;
 public class Gambler {
     public static final int $DAILY = 100;
     public static final int $PERGAME = 1;
+    public static final int WIN = 1;
+    public static final int LOSE = 0;
     public static void main(String[] args) {
+        double Action = Math.floor(Math.random() * 10) % 2;
+        if(Action==WIN)
+        {
+            System.out.println("You won $1");
+
+        }
+        else if(Action==LOSE)
+        {
+            System.out.println("You lose $1");
+        }
 
     }
 }
