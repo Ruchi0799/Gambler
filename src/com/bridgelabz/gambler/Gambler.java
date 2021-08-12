0896483fba22939029acb6cc4f8a3617148966a5
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 12:37:00 +0530

    [Ruchi]: Added initialisation variables

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.gambler;
 
 public class Gambler {
-    
+    public static final int $DAILY = 100;
+    public static final int $PERGAME = 1;
+    public static void main(String[] args) {
+
+    }
 }
